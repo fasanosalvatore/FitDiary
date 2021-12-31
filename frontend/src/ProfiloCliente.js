4d80aeef8da136940a32b4948a25ce65b8700903
Daniele Giaquinto <giaqui@gmail.com>
2021-12-31 11:49:26 +0100

    feat(frontend): add ProfiloCliente component, wip
    also add fakeBackend.js to handle fake response from backend

---
+++ frontend/src/ProfiloCliente.js
@@ -0,0 +1,57 @@
+import {
+    Avatar,
+    Box, Button,
+    Divider,
+    Flex,
+    Grid,
+    GridItem,
+    Heading,
+    HStack, Icon,
+    Image,
+    SimpleGrid,
+    Text,
+    VStack
+} from "@chakra-ui/react";
+import {getUser} from "./fakeBackend";
+
+export default function ProfiloCliente() {
+    const utente = getUser().data.utente;
+    return (
+        <Flex wrap={"wrap"}>
+            <Heading w={"full"} mb={5}>Profilo Utente</Heading>
+            <Box bg={"blackAlpha.50"} rounded={20} padding={10} minW={"350"}>
+                <Flex>
+                    <VStack w={"full"}>
+                        <Avatar size={"xl"}></Avatar>
+                        <Heading fontSize={"3xl"} color={utente.sesso == "M" ? "blue.700" : "pink.700"} >{utente.nome} {utente.cognome}</Heading>
+                        <Text color={"gray.400"}>{utente.email}</Text>
+                        <HStack><Button bg={"blue.200"}>Modifica</Button><Button bg={"green.200"}>Progressi</Button></HStack>
+                    </VStack>
+                </Flex>
+            </Box>
+            <Box bg={"blackAlpha.50"} rounded={20} padding={10} minW={"450"} marginLeft={[0,0,0,5]} marginTop={[5,5,5,0]}>
+                <Flex>
+                    <VStack w={"full"} alignItems={"flex-start"}>
+                        <SimpleGrid columns={2} w={"full"}>
+                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text fontWeight={"bold"} color={"gray.600"}>Data di Nascita </Text></GridItem>
+                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text ml={"2"} color={"gray.400"}>{utente.dataNascita}</Text></GridItem>
+                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text fontWeight={"bold"} color={"gray.600"}>Telefono </Text></GridItem>
+                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text ml={"2"} color={"gray.400"}>{utente.telefono}</Text></GridItem>
+                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text fontWeight={"bold"} color={"gray.600"}>Indirizzo</Text></GridItem>
+                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text ml={"2"} color={"gray.400"}>{utente.via} <br/>{utente.cap} {utente.citta}</Text></GridItem>
+                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text fontWeight={"bold"} color={"gray.600"}>Preparatore </Text></GridItem>
+                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text ml={"2"} color={"gray.400"}>{utente.preparatore.nome} {utente.preparatore.cognome}</Text></GridItem>
+                        </SimpleGrid>
+                    </VStack>
+                </Flex>
+            </Box>
+            <Box bg={"blackAlpha.50"} rounded={20} padding={10} minW={"350"} mt={5} w={"full"}>
+                <Flex>
+                    <VStack>
+                        <Icon></Icon>
+                    </VStack>
+                </Flex>
+            </Box>
+        </Flex>
+    )
+}
---
+++ frontend/src/fakeBackend.js
@@ -0,0 +1,49 @@
+let user = {
+    "data": {
+        "utente": {
+            "id": 4,
+            "nome": "John",
+            "cognome": "Dohe",
+            "email": "giaqui@gmail.com",
+            "password": "$2a$10$cDlRv6/3qzj79BmSU4X.Hu5rcyUT6v4oczfrwJFCC/APgMajQugMu",
+            "attivo": false,
+            "dataNascita": "2000-12-12",
+            "dataCreazione": "2021-01-01T00:00:01",
+            "dataAggiornamento": "2021-12-31T00:10:18",
+            "sesso": "M",
+            "telefono": "3347274753",
+            "via": "Corso Diaz, 140",
+            "cap": "84084",
+            "citta": "Fisciano (NA)",
+            "preparatore": {
+                "id": 1,
+                "nome": "Rambo",
+                "cognome": "Potente",
+                "email": "iosonorambo@rambo.com",
+                "password": "$2a$10$cDlRv6/3qzj79BmSU4X.Hu5rcyUT6v4oczfrwJFCC/APgMajQugMu",
+                "attivo": true,
+                "dataNascita": "1990-12-12",
+                "dataCreazione": "2021-01-01T00:00:01",
+                "dataAggiornamento": "2021-12-31T00:10:18",
+                "sesso": "M",
+                "telefono": "3365895875",
+                "via": null,
+                "cap": null,
+                "citta": null,
+            },
+            "ruolo": {
+                "id": 1,
+                "nome": "PREPARATORE",
+                "dataCreazione": "2021-12-31T00:10:18",
+                "dataAggiornamento": "2021-12-31T00:10:18"
+            },
+            "listaProtocolli": null,
+            "listaReport": null
+        },
+    },
+    "status": "success"
+};
+
+export function getUser() {
+    return user;
+}
