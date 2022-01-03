e3b237eb7177c242d9839120a72736ce58b2157a
Everysimo <simone.spera.13@gmail.com>
2022-01-03 01:29:37 +0100

    fix(frontend): fix user data handling

--- frontend/src/pages/User/Customer/CustomerProfile.jsx
+++ frontend/src/pages/User/Customer/CustomerProfile.jsx
@@ -9,10 +9,11 @@ import {
     Text,
     VStack
 } from "@chakra-ui/react";
-import {getUser} from "../../../fakeBackend";
+import authService from "../../../services/auth.service";
 
 export default function CustomerProfile() {
-    const utente = getUser().data.utente;
+    const utente = authService.getCurrentUser().utente;
+    console.log(utente);
     return (
         <Flex wrap={"wrap"}>
             <Heading w={"full"} mb={5}>Profilo Utente</Heading>
@@ -26,22 +27,35 @@ export default function CustomerProfile() {
                     </VStack>
                 </Flex>
             </Box>
-            <Box bg={"blackAlpha.50"} rounded={20} padding={10} minW={"450"} marginLeft={[0,0,0,5]} marginTop={[5,5,5,0]}>
-                <Flex>
-                    <VStack w={"full"} alignItems={"flex-start"}>
-                        <SimpleGrid columns={2} w={"full"}>
-                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text fontWeight={"bold"} color={"gray.600"}>Data di Nascita </Text></GridItem>
-                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text ml={"2"} color={"gray.400"}>{utente.dataNascita}</Text></GridItem>
-                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text fontWeight={"bold"} color={"gray.600"}>Telefono </Text></GridItem>
-                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text ml={"2"} color={"gray.400"}>{utente.telefono}</Text></GridItem>
-                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text fontWeight={"bold"} color={"gray.600"}>Indirizzo</Text></GridItem>
-                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text ml={"2"} color={"gray.400"}>{utente.via} <br/>{utente.cap} {utente.citta}</Text></GridItem>
-                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text fontWeight={"bold"} color={"gray.600"}>Preparatore </Text></GridItem>
-                            <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text ml={"2"} color={"gray.400"}>{utente.preparatore.nome} {utente.preparatore.cognome}</Text></GridItem>
-                        </SimpleGrid>
-                    </VStack>
-                </Flex>
-            </Box>
+            {utente.preparatore ?
+                <Box bg={"blackAlpha.50"} rounded={20} padding={10} minW={"450"} marginLeft={[0, 0, 0, 5]}
+                     marginTop={[5, 5, 5, 0]}>
+                    <Flex>
+                        <VStack w={"full"} alignItems={"flex-start"}>
+                            <SimpleGrid columns={2} w={"full"}>
+                                <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text
+                                    fontWeight={"bold"} color={"gray.600"}>Data di Nascita </Text></GridItem>
+                                <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text
+                                    ml={"2"} color={"gray.400"}>{utente.dataNascita}</Text></GridItem>
+                                <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text
+                                    fontWeight={"bold"} color={"gray.600"}>Telefono </Text></GridItem>
+                                <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text
+                                    ml={"2"} color={"gray.400"}>{utente.telefono}</Text></GridItem>
+                                <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text
+                                    fontWeight={"bold"} color={"gray.600"}>Indirizzo</Text></GridItem>
+                                <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text
+                                    ml={"2"} color={"gray.400"}>{utente.via} <br/>{utente.cap} {utente.citta}
+                                </Text></GridItem>
+                                <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text
+                                    fontWeight={"bold"} color={"gray.600"}>Preparatore </Text></GridItem>
+                                <GridItem minH={"5"} p={"3"} borderBottom={"1px"} borderColor={"gray.400"}><Text
+                                    ml={"2"}
+                                    color={"gray.400"}>{utente.preparatore.nome} {utente.preparatore.cognome}</Text></GridItem>
+                            </SimpleGrid>
+                        </VStack>
+                    </Flex>
+                </Box>
+            : <Box></Box>}
             <Box bg={"blackAlpha.50"} rounded={20} padding={10} minW={"350"} mt={5} w={"full"}>
                 <Flex>
                     <VStack>
