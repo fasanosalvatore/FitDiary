f1ac1c03135e421a41aaae56633f54f0d6f76c83
Everysimo <simone.spera.13@gmail.com>
2022-01-05 01:35:48 +0100

    fix(frontend): fix fields for edit customer

--- frontend/src/pages/User/Customer/CustomerEditInfo.jsx
+++ frontend/src/pages/User/Customer/CustomerEditInfo.jsx
@@ -12,16 +12,20 @@ import {
     SimpleGrid,
     Stack, Text, Tooltip, useBreakpointValue, VStack
 } from "@chakra-ui/react";
-import {getUserFull} from "../../../fakeBackend";
+import {getCurrentUser, getUserFull} from "../../../fakeBackend";
 import config from "../../../config.json";
+import authservice from "../../../services/auth.service";
+import axios from "axios";
 
 
 export default function CustomerEditInfo() {
     const urlEditInfo = `${config.SERVER_URL}/utenti/cliente`;
     const {register, handleSubmit, setValue, formState: {errors, isSubmitting}} = useForm();
     const colSpan = useBreakpointValue({base: 2, md: 1})
+    const [currentUser,setCurrentUser] = /*useState(AuthService.getCurrentUser())*/useState(authservice.getCurrentUser())
 
-    const resp = getUserFull();
+    const resp = currentUser;
+    console.log(resp)
 
     function handleResponse(response) {
         console.log("handling");
@@ -40,8 +44,8 @@ export default function CustomerEditInfo() {
     }
 
     useEffect(() => {
-        const fields = ['dataNascita', 'telefono', 'via', 'cap', 'citta'];
-        fields.forEach(field => setValue(field, resp.data.utente[field]));
+        const fields = ['nome','cognome','email','dataNascita', 'telefono', 'via', 'cap', 'citta'];
+        fields.forEach(field => setValue(field, currentUser.utente[field]));
     })
 
 
@@ -49,30 +53,92 @@ export default function CustomerEditInfo() {
     //Chiamata API inserimento dati personali utente
     function onSubmit(values) {
 
-        console.log("submitting ");
-        const requestOptions = {
-            method: "PUT",
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(values)
-        }
-        return fetch(urlEditInfo, requestOptions)
-            .then(handleResponse)
+        console.log("submitting values");
+        console.log(values);
+        console.log(currentUser.access_token)
+        return axios({
+                url:urlEditInfo,
+                method: "PUT",
+                headers: {'Content-Type': 'application/json',
+                'Authorization': "Bearer "+currentUser.access_token},
+                body: JSON.stringify(values)
+        }).then(handleResponse)
             .catch(handleFail)
     }
-
-    console.log(errors);
 
     function isValidDate(value) {
         return (!isNaN(Date.parse(value)) && (new Date(value) < Date.now()) ? true : "Inserisci una data valida");
     }
-
+    //nome,cognome,email,password,dataNascita
     return (
         <VStack w="full" h="full" p={[5, 10, 20]}>
             <VStack spacing={3} alignItems="flex-start" pb={5}>
-                <Heading size="lg">Inserimento Dati Personali</Heading>
+                <Heading size="lg">Modifica Dati Personali</Heading>
             </VStack>
             <form style={{width: "100%"}} onSubmit={handleSubmit(onSubmit)}>
-                <SimpleGrid>
+                <SimpleGrid vcolumns={2} columnGap={5} rowGap={5} w="full">
+                    <GridItem colSpan={colSpan} w="100%">
+                        <FormControl id={"nome"} isInvalid={errors.nome}>
+                            <FormLabel htmlFor="nome">Nome</FormLabel>
+                            <Input type="text" placeholder="Mario" {...register("nome", {
+                                required: "Il nome è obbligatorio",
+                                maxLength: {value: 50, message: "Il nome è troppo lungo"},
+                                pattern: {value: /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/i, message: "Formato nome non valido"}
+                            })} />
+                            <FormErrorMessage>{errors.nome && errors.nome.message}</FormErrorMessage>
+                        </FormControl>
+                    </GridItem>
+                    <GridItem colSpan={colSpan}>
+                        <FormControl id={"cognome"} isInvalid={errors.cognome}>
+                            <FormLabel>Cognome</FormLabel>
+                            <Input type="text" placeholder="Rossi" {...register("cognome", {
+                                required: "Il cognome è obbligatorio",
+                                maxLength: {value: 50, message: "Il cognome è troppo lungo"},
+                                pattern: {value: /^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$/i, message: "Formato cognome non valido"}
+                            })} />
+                            <FormErrorMessage>{errors.cognome && errors.cognome.message}</FormErrorMessage>
+                        </FormControl>
+                    </GridItem>
+
+                    <GridItem colSpan={2} w="100%">
+                        <FormControl id={"dataNascita"} isInvalid={errors.dataNascita}>
+                            <FormLabel>Data di Nascita</FormLabel>
+                            <Input type="date" placeholder="2001-01-05" {...register("dataNascita", {
+                                required: "La data di nascita è obbligatoria",
+                                validate: value => {
+                                    return isValidDate(value)
+                                }
+                            })} />
+                            <FormErrorMessage>{errors.dataNascita && errors.dataNascita.message}</FormErrorMessage>
+                        </FormControl>
+                    </GridItem>
+                    <GridItem colSpan={2}>
+                        <FormControl id={"email"} isInvalid={errors.email}>
+                            <FormLabel>Email</FormLabel>
+                            <Input type="text" placeholder="Email" {...register("email", {
+                                required: "Il campo email è obbligatorio",
+                                pattern: {value: /^\S+@\S+$/i, message: "Formato email non valido"}
+                            })} />
+                            <FormErrorMessage>{errors.email && errors.email.message}</FormErrorMessage>
+                        </FormControl>
+                    </GridItem>
+                    <GridItem colSpan={2}>
+                        <FormControl id={"password"} isInvalid={errors.password}>
+                            <FormLabel>Password</FormLabel>
+                            <Tooltip
+                                label={"La password deve contenere: Una lettera maiuscola, minuscola, un numero e un carattere speciale"}
+                                aria-label='A tooltip'>
+                                <Input type="password" placeholder="Password" {...register("password", {
+                                    maxLength: {value: 255, message: "Password troppo lunga"},
+                                    pattern: {
+                                        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?^#()<>+&.])[A-Za-z\d@$!%*?^#()<>+&.]{8,}$/i,
+                                        message: "Formato password non valido"
+                                    }
+                                })} />
+                            </Tooltip>
+                            <FormErrorMessage>{errors.password && errors.password.message}</FormErrorMessage>
+                        </FormControl>
+                    </GridItem>
                     {/*
                     <GridItem colSpan={colSpan} w="100%">
                         <FormControl id={"altezza"} isInvalid={errors.altezza}>
@@ -92,20 +158,7 @@ export default function CustomerEditInfo() {
                         </FormControl>
                     </GridItem>*/}
 
-                    <GridItem colSpan={colSpan} w="100%">
-                        <FormControl id={"dataNascita"} isInvalid={errors.dataNascita}>
-                            <FormLabel>Data di Nascita</FormLabel>
-                            <Input type="date" placeholder="2001-01-05" {...register("dataNascita", {
-                                required: "La data di nascita è obbligatoria",
-                                validate: value => {
-                                    return isValidDate(value)
-                                }
-                            })} />
-                            <FormErrorMessage>{errors.dataNascita && errors.dataNascita.message}</FormErrorMessage>
-                        </FormControl>
-                    </GridItem>
-
-                    <GridItem colSpan={colSpan} w="100%">
+                    <GridItem colSpan={2} w="100%">
                         <FormControl id={"telefono"} isInvalid={errors.telefono}>
                             <FormLabel>Numero di telefono</FormLabel>
                             <Input type="text" placeholder="3332957615"{...register("telefono", {
@@ -126,7 +179,7 @@ export default function CustomerEditInfo() {
                         </FormControl>
                     </GridItem>
 
-                    <GridItem colSpan={colSpan} w="100%">
+                    <GridItem colSpan={2} w="100%">
                         <FormControl id={"citta"} isInvalid={errors.citta}>
                             <FormLabel htmlFor="citta"> Città</FormLabel>
                             <Input type="text" placeholder="Roma"{...register("citta", {
@@ -163,7 +216,7 @@ export default function CustomerEditInfo() {
                             <FormErrorMessage>{errors.cap && errors.cap.message}</FormErrorMessage>
                         </FormControl>
                     </GridItem>
-                    <GridItem colSpan={colSpan} w="100%">
+                    <GridItem colSpan={2} w="100%">
                         <Button size="lg" w="full" mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
                             Modifica dati Personali
                         </Button>
