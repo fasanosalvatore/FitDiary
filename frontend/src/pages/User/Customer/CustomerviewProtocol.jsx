f4506b25aabd8f659d8e572492047d353493a585
lnrdmnc <jlrknumber1@gmail.com>
2022-01-06 03:10:56 +0100

    fix(frontend): add table and attribute

--- frontend/src/pages/User/Customer/CustomerviewProtocol.jsx
+++ frontend/src/pages/User/Customer/CustomerviewProtocol.jsx
@@ -5,52 +5,97 @@ import {
     Text,
     Avatar,
     Flex, VStack, HStack, Button, FormLabel, Grid, GridItem, SimpleGrid, Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
 } from '@chakra-ui/react';
+
 import {
     RiArrowGoBackLine,
 } from 'react-icons/ri';
 import authService from "../../../services/auth.service";
+import {getProtocollo} from "../../../fakeBackend";
+import moment from "moment";
+
+
 export default function CustomerviewProtocol() {
     /**const utente = authService.getCurrentUser().utente;**/
+    const protocollo=getProtocollo().data;
+    moment.locale("it-IT");
+    return (
 
-        return(
+        <Flex wrap={"wrap"}>
+            <Button leftIcon={<RiArrowGoBackLine/>}>Torna alla lista</Button>
+            <Heading w={"full"} mb={5} textAlign={"center"}>Protocollo n.{protocollo.protocollo.id}</Heading>
+            <Box bg={"blackAlpha.50"} rounded={20} padding={10} minW={"full"} height={500}>
+                <Flex>
+                    <VStack w="full" h="full" align="start">
+                        <HStack w="full" h="full" align="start">
+                            <Flex width="full" justify="space-between">
+                                <HStack>
+                                    <Heading size={"sx"} textAlign="start"> Data inizio:</Heading>
+                                    <Text>{moment(protocollo.protocollo.dataCreazione).format("DD/MM/yyyy")}</Text>
+                                </HStack>
+                                <HStack>
+                                    <Heading size={"sx"} textAlign="center"> Nome preparatore:</Heading>
+                                    <Text>{protocollo.protocollo.preparatore.nome} {protocollo.protocollo.preparatore.cognome}</Text>
+                                </HStack>
+                                <HStack>
+                                    <Heading size={"sx"} textAlign="center">Data fine:</Heading>
+                                    <Text>{moment(protocollo.protocollo.dataScadenza).format("DD/MM/yyyy")}</Text>
+                                </HStack>
+                            </Flex>
+                        </HStack>
+                        <HStack w="full" h="full" align="start">
+                            <Flex width="full" justify="space-between">
+                                <HStack alignItems="center">
+                                    <Box backgroundColor={"white"} p={2} borderRadius={15}>
+                                        <Table variant={"striped"}  colorScheme={"gray"} size="md">
+                                            <TableCaption>PRIMA</TableCaption>
+                                            <Thead color>
+                                                <Tr>
+                                                    <Th></Th>
+                                                    <Th></Th>
+                                                </Tr>
+                                            </Thead>
+                                            <Tbody>
+                                                <Tr>
+                                                    <Td>Peso</Td>
+                                                    <Td>{}Kg</Td>
+                                                </Tr>
+                                                <Tr>
+                                                    <Td>Circonferenza Bicipite</Td>
+                                                    <Td>{}cm</Td>
+                                                </Tr>
+                                                <Tr>
+                                                    <Td>Circonferenza Addome</Td>
+                                                    <Td>{}cm</Td>
+                                                </Tr>
+                                                <Tr>
+                                                    <Td>Circonferenza Quadricipite</Td>
+                                                    <Td>{}cm</Td>
+                                                </Tr>
+                                            </Tbody>
+
+                                        </Table>
+                                    </Box>
+                                </HStack>
+                                <HStack>
+                                    <Heading size={"md"} textAlign="center">Dopo</Heading>
+                                </HStack>
+                            </Flex>
+                        </HStack>
+                    </VStack>
+                </Flex>
+            </Box>
+
+        </Flex>
 
 
-
-            <Flex wrap={"wrap"}>
-                <Button leftIcon={<RiArrowGoBackLine/>}>Torna alla lista</Button>
-                <Heading w={"full"} mb={5} textAlign={"center"}>Protocollo n.{}</Heading>
-                <Box bg={"blackAlpha.50"} rounded={20} padding={10} minW={"full"}  height={500} >
-                    <Flex>
-                        <VStack w="full" h="full" align="start" >
-                            <SimpleGrid column={3} row={1} w="full" >
-
-                                <GridItem colStart={1} textAlign="start">
-                                    <Heading size={"xs"} >Data inizio: </Heading><Text>09/10/2021</Text>
-                                </GridItem>
-
-                                <GridItem  colStart={2} textAlign="center">
-                                    <Heading size={"xs"}>Nome preparatore: </Heading><Text>Mario Rossi</Text>
-                                </GridItem>
-
-                                <GridItem colStart={3} textAlign="end">
-                                    <Heading size={"xs"}>Data fine: </Heading><Text>09/11/2021</Text>
-                                </GridItem>
-
-                            </SimpleGrid>
-
-                            <Table>
-
-
-
-                            </Table>
-
-                        </VStack>
-                    </Flex>
-                </Box>
-
-            </Flex>
-
-
-);
+    )
+        ;
 }
