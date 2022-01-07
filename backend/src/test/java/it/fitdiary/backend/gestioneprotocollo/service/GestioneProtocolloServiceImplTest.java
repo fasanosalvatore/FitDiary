602058450972949b1cc5e9aa9438fd1c0a0bd012
rebeccadimatteo <81223461+rebeccadimatteo@users.noreply.github.com>
2022-01-07 02:02:02 +0100

    feat(backend): create test for CreazioneProtocollo in  GestioneProtocolloServiceImplTest

--- backend/src/test/java/it/fitdiary/backend/gestioneprotocollo/service/GestioneProtocolloServiceImplTest.java
+++ backend/src/test/java/it/fitdiary/backend/gestioneprotocollo/service/GestioneProtocolloServiceImplTest.java
@@ -6,6 +6,7 @@ import it.fitdiary.backend.entity.Ruolo;
 import it.fitdiary.backend.entity.SchedaAlimentare;
 import it.fitdiary.backend.entity.SchedaAllenamento;
 import it.fitdiary.backend.entity.Utente;
+import it.fitdiary.backend.gestioneprotocollo.repository.AlimentoRepository;
 import it.fitdiary.backend.gestioneprotocollo.repository.ProtocolloRepository;
 import it.fitdiary.backend.gestioneutenza.repository.RuoloRepository;
 import it.fitdiary.backend.gestioneutenza.repository.UtenteRepository;
@@ -17,17 +18,22 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = BackendApplicationTest.class)
@@ -42,31 +48,80 @@ class GestioneProtocolloServiceImplTest {
     private Utente cliente;
     private Utente clienteAggiornato;
     private Utente preparatore;
-    private  Utente updatedPreparatore;
+    private Utente updatedPreparatore;
     private Protocollo protocollo;
+    @Mock
+    private AlimentoRepository alimentoRepository;
+
     @Before
     public void setUp() {
         ruoloCliente = new Ruolo(3L, "CLIENTE", null, null);
         ruoloPreparatore = new Ruolo(2L, "PREPARATORE", null, null);
-        cliente = new Utente(1L, "Rebecca", "Di Matteo", "beccadimatteoo@gmail.com", "Becca123*", true, null, null, null,
-                null, null, null, null, ruoloCliente, null, null, null, null, null);
-        clienteAggiornato = new Utente(1L, "Rebecca", "Di Matteo", "beccadimatteoo@gmail.com", "Becca123*", true,
-                LocalDate.parse("2000-10-30"), null, "3894685921", "Francesco rinaldo", "94061", "Agropoli", null,
+        cliente = new Utente(1L, "Rebecca", "Di Matteo",
+                "beccadimatteoo@gmail.com", "Becca123*", true, null, null, null,
+                null, null, null, null, ruoloCliente, null, null, null, null,
+                null);
+        clienteAggiornato = new Utente(1L, "Rebecca", "Di Matteo",
+                "beccadimatteoo@gmail.com", "Becca123*", true,
+                LocalDate.parse("2000-10-30"), null, "3894685921",
+                "Francesco rinaldo", "94061", "Agropoli", null,
                 ruoloCliente, null, null, null, null, null);
         preparatore =
-                new Utente(1L, "Daniele", "De Marco", "diodani5@gmail.com", "Trappo#98", true, null, null, null, null,
-                        null, null, null, ruoloPreparatore, null, null, null, null, null);
-        updatedPreparatore = new Utente(1L, "Michele", "De Marco", "diodani5@gmail.com", "Trappo#98", true,
-                LocalDate.parse("2000-03-03"), null, "3459666587", "Francesco La Francesca", "84126", "Salerno", null,
-                ruoloPreparatore, null, null, null, null, null);
-protocollo=new Protocollo(1L,LocalDate.parse("2022-01-05"),new SchedaAlimentare(),new SchedaAllenamento(),cliente,preparatore,null,null);
+                new Utente(1L, "Daniele", "De Marco", "diodani5@gmail.com",
+                        "Trappo#98", true, null, null, null, null,
+                        null, null, null, ruoloPreparatore, null, null, null,
+                        null, null);
+        updatedPreparatore =
+                new Utente(1L, "Michele", "De Marco", "diodani5@gmail.com",
+                        "Trappo#98", true,
+                        LocalDate.parse("2000-03-03"), null, "3459666587",
+                        "Francesco La Francesca", "84126", "Salerno", null,
+                        ruoloPreparatore, null, null, null, null, null);
+        protocollo = new Protocollo(1L, LocalDate.parse("2022-01-05"),
+                new SchedaAlimentare(), new SchedaAllenamento(), cliente,
+                preparatore, null, null);
+
     }
 
     @Test
     void visualizzaStoricoProtocolliCliente() {
-        List<Protocollo> protocolloList=new ArrayList<Protocollo>();
+        List<Protocollo> protocolloList = new ArrayList<Protocollo>();
         protocolloList.add(protocollo);
-        when(protocolloRepository.findAllByCliente(cliente)).thenReturn(protocolloList);
-        assertEquals(protocolloList,gestioneProtocolloServiceImpl.visualizzaStoricoProtocolliCliente(cliente));
+        when(protocolloRepository.findAllByCliente(cliente)).thenReturn(
+                protocolloList);
+        assertEquals(protocolloList,
+                gestioneProtocolloServiceImpl.visualizzaStoricoProtocolliCliente(
+                        cliente));
+    }
+
+    @Test
+    void creazioneProtocolloSchedeNulle() throws IOException {
+
+        assertThrows(IllegalArgumentException.class,
+                () -> gestioneProtocolloServiceImpl.creazioneProtocollo(
+                        protocollo, null, null));
+    }
+
+    @Test
+    void creazioneProtocolloOnSuccess() throws IOException {
+        Protocollo protocolloSuccess =
+                new Protocollo(2L, LocalDate.parse("2022-01-07"),
+                        new SchedaAlimentare(), new SchedaAllenamento(), null,
+                        null, null, null);
+        File schedaAlimentare = new File(
+                getClass().getClassLoader().getResource("schedaAlimentare.csv")
+                        .getFile());
+        File schedaAllenamento = new File(
+                getClass().getClassLoader().getResource("schedaAllenamento.csv")
+                        .getFile());
+        when(mock(GestioneProtocolloServiceImpl.class).inserisciSchedaAlimentare(
+                protocolloSuccess,
+                schedaAlimentare)).thenReturn(protocolloSuccess);
+        when(mock(GestioneProtocolloServiceImpl.class).inserisciSchedaAllenamento(
+                protocolloSuccess,
+                schedaAllenamento)).thenReturn(protocolloSuccess);
+        assertEquals(protocolloSuccess,
+                gestioneProtocolloServiceImpl.creazioneProtocollo(protocollo,
+                        schedaAlimentare, schedaAllenamento));
     }
 }
