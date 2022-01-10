95f6b4cad5ca9697bfa03b6487a7cd7d05e0a147
Davide La Gamba <davidelg06@gmail.com>
2022-01-11 00:13:26 +0100

    fix(backend): fix annotations for entities ImmaginiReport and Report

--- backend/src/main/java/it/fitdiary/backend/entity/Report.java
+++ backend/src/main/java/it/fitdiary/backend/entity/Report.java
@@ -1,8 +1,9 @@
 package it.fitdiary.backend.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -20,7 +21,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Report {
