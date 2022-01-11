2c60f81dc722897e423d59c2b33a3076561ce5f1
Daniele Giaquinto <giaqui@gmail.com>
2022-01-11 01:59:13 +0100

    stick footer on the bottom of the screen

--- frontend/src/AppShell.jsx
+++ frontend/src/AppShell.jsx
@@ -1,16 +1,17 @@
 import SidebarWithHeader from "./components/SidebarWithHeader";
 import React from "react";
 import Footer from "./components/Footer";
+import { Box, calc, Container } from "@chakra-ui/react";
 
 const AppShell = ({children}) => {
     return (
-        <>
-            <SidebarWithHeader>
-                {children}
-                <Footer/>
-            </SidebarWithHeader>
-        </>
-    )
+      <>
+        <SidebarWithHeader>
+            {children}
+        </SidebarWithHeader>
+        <Footer />
+      </>
+    );
 }
 
 export default AppShell;
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,28 +1,58 @@
-import {ButtonGroup, Flex, IconButton, Link, Stack, Text,} from '@chakra-ui/react';
-import {FaFacebook, FaInstagram} from 'react-icons/fa';
+import {
+    ButtonGroup,
+    Flex,
+    IconButton,
+    Link,
+    Stack,
+    Text,
+} from "@chakra-ui/react";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 import React from "react";
-import Logo from './Logo';
+import Logo from "./Logo";
 
 export default function Footer() {
     return (
         <Flex
-            px={[0, 5, 10]} h={{base: "25vh", md: "20vh"}} bg={"blue.500"} as="footer" mx="auto"
-            direction={{base: 'column', md: 'row'}}
+            ml={{ base: 0, md: 60 }}
+            px={[0, 5, 10]}
+            minHeight="20vh"
+            bg={"blue.500"}
+            as="footer"
+            mx="auto"
+            direction={{ base: "column", md: "row" }}
             spacing={4}
-            justify={{base: 'center', md: 'space-between'}}
-            align={{base: 'center', md: 'center'}}>
+            justify={{ base: "center", md: "space-between" }}
+            align={{ base: "center", md: "center" }}
+        >
             <Link href={"/"}>
-                <Logo viewBox="-10 -50 250 250" color="white" boxSize={{ base: "8em", md: "6em"}}/>
+                <Logo
+                    viewBox="-10 -50 250 250"
+                    color="white"
+                    boxSize={{ base: "4em", md: "6em" }}
+                />
             </Link>
-            <Text color={"white"} mb={[0, 5]}>© 2022 FitDiary. All rights reserved</Text>
-            <Stack direction={{base: 'column', lg: 'row'}} spacing={{base: '10', lg: '28'}}>
+            <Text color={"white"} mb={[0, 2]}>
+                © 2022 FitDiary. All rights reserved
+            </Text>
+            <Stack
+                direction={{ base: "column", lg: "row" }}
+                spacing={{ base: "10", lg: "28" }}
+            >
                 <ButtonGroup colorScheme={"gray"}>
-                    <IconButton colorScheme={"facebook"} as="a" href="https://www.facebook.com/fitdiary21"
-                                aria-label="Facebook"
-                                icon={<FaFacebook fontSize="20px"/>}/>
-                    <IconButton colorScheme={"red"} as="a" href="https://www.instagram.com/fitdiary_is"
-                                aria-label="Facebook"
-                                icon={<FaInstagram fontSize="20px"/>}/>
+                    <IconButton
+                        colorScheme={"facebook"}
+                        as="a"
+                        href="https://www.facebook.com/fitdiary21"
+                        aria-label="Facebook"
+                        icon={<FaFacebook />}
+                    />
+                    <IconButton
+                        colorScheme={"red"}
+                        as="a"
+                        href="https://www.instagram.com/fitdiary_is"
+                        aria-label="Facebook"
+                        icon={<FaInstagram fontSize="20px" />}
+                    />
                 </ButtonGroup>
             </Stack>
         </Flex>
