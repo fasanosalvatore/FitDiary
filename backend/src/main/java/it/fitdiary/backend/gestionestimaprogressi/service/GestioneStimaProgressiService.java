486b8f86338cb11dec7f346c9fd85d196b642d07
Antonio <trapaneseantonio1@gmail.com>
2022-01-16 00:17:07 +0100

    feat(backend): add GestioneStimaProgressiService

---
+++ backend/src/main/java/it/fitdiary/backend/gestionestimaprogressi/service/GestioneStimaProgressiService.java
@@ -0,0 +1,7 @@
+package it.fitdiary.backend.gestionestimaprogressi.service;
+
+import it.fitdiary.backend.entity.Report;
+
+public interface GestioneStimaProgressiService {
+    Report generazioneStimaProgressi(Report report);
+}
