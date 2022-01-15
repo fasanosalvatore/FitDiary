96833f433d011e84790a3939bc3ea7448d7e0d17
rebeccadimatteo <81223461+rebeccadimatteo@users.noreply.github.com>
2022-01-16 00:07:42 +0100

    fix(backend): fix StimaProgressiAdapter

--- backend/src/main/java/it/fitdiary/backend/gestionestimaprogressi/adapter/StimaProgressiAdapter.java
+++ backend/src/main/java/it/fitdiary/backend/gestionestimaprogressi/adapter/StimaProgressiAdapter.java
@@ -3,5 +3,10 @@ package it.fitdiary.backend.gestionestimaprogressi.adapter;
 import it.fitdiary.backend.entity.Report;
 
 public interface StimaProgressiAdapter {
+    /**
+     *
+     * @param report il nuovo report del cliente
+     * @return report con aggiunta peso stimato
+     */
     Report calcolaStimaProgressiReport(Report report);
 }
