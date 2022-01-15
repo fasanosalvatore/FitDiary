7fd281b35381c2b50b392edc113db37e5b88d284
rebeccadimatteo <81223461+rebeccadimatteo@users.noreply.github.com>
2022-01-15 23:58:55 +0100

    feat(backend): create StimaProgressiAdapter

---
+++ backend/src/main/java/it/fitdiary/backend/gestionestimaprogressi/adapter/StimaProgressiAdapter.java
@@ -0,0 +1,7 @@
+package it.fitdiary.backend.gestionestimaprogressi.adapter;
+
+import it.fitdiary.backend.entity.Report;
+
+public interface StimaProgressiAdapter {
+    Report calcolaStimaProgressiReport(Report report);
+}
