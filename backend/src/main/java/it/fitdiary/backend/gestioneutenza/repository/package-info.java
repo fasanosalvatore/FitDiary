f6559d7f6f3495feb6d27f26dd7b2e8721e015d7
Davide La Gamba <davidelg06@gmail.com>
2022-01-19 00:08:47 +0100

    fix(backend): fix package-info.java

--- backend/src/main/java/it/fitdiary/backend/gestioneutenza/repository/package-info.java
+++ backend/src/main/java/it/fitdiary/backend/gestioneutenza/repository/package-info.java
@@ -1,13 +1,9 @@
 /**
- * This module is about impact of the final keyword on performance
- * <p>
- * This module explores  if there are any performance benefits from
- * using the final keyword in our code. This module examines the performance
- * implications of using final on a variable, method, and class level.
- * </p>
+ * gestisce la persistenza degli oggetti entity ruolo e utente.
  *
  * @since 1.0
- * @author baeldung
- * @version 1.1
+ * @author Daniele De Marco, Rebecca Di Matteo, Davide La Gamba, Antonio
+ * Trapanese
+ * @version 1.0
  */
 package it.fitdiary.backend.gestioneutenza.repository;
