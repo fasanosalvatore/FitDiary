5a5a2d77ef29a734b3d32823119983502f11f7a0
dany98sa <56975152+dany98sa@users.noreply.github.com>
2022-01-18 19:53:41 +0100

    feat(backend): add seach report for date of creation

--- backend/src/main/java/it/fitdiary/backend/gestionereport/repository/ReportRepository.java
+++ backend/src/main/java/it/fitdiary/backend/gestionereport/repository/ReportRepository.java
@@ -3,7 +3,9 @@ package it.fitdiary.backend.gestionereport.repository;
 import it.fitdiary.backend.entity.Report;
 import it.fitdiary.backend.entity.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
@@ -12,4 +14,15 @@ public interface ReportRepository extends JpaRepository<Report, Long> {
      * @return lista dei report del cliente
      */
     List<Report> findAllByCliente(Utente cliente);
+
+    /**
+     * cerca report per data.
+     * @param idCliente id cliente
+     * @param dataCreazione data creazione report
+     * @return report
+     */
+    Report findFirstByCliente_IdAndDataCreazioneIsBeforeOrderByDataCreazioneDesc
+    (
+            Long idCliente, LocalDateTime dataCreazione
+    );
 }
