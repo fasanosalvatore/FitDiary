71d0ec1f85848af847169d623da2eebc140e9a4e
Everysimo <simone.spera.13@gmail.com>
2023-09-12 20:54:18 +0200

    fix: start protocol Cretion

--- frontend/src/pages/Protocols/Create.jsx
+++ frontend/src/pages/Protocols/Create.jsx
@@ -1,32 +1,52 @@
 import {
-    Box,
-    Button,
-    Flex,
-    FormControl,
-    FormErrorMessage,
-    FormLabel,
-    GridItem,
-    Heading,
-    HStack,
-    Input,
-    SimpleGrid,
-    Text,
-    useToast,
-    VStack
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  GridItem,
+  Heading,
+  HStack,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  SimpleGrid,
+  Table,
+  TableCaption,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr, useDisclosure,
+  useToast,
+  VStack
 } from "@chakra-ui/react";
 import {useForm} from "react-hook-form"
+import {Link as ReactLink} from "react-router-dom";
 import React, {useCallback, useContext, useEffect, useState} from "react";
 import {useDropzone} from 'react-dropzone';
 import {FetchContext} from "../../context/FetchContext";
 import { GiMeal } from "react-icons/gi";
 import {IoIosFitness} from "react-icons/io";
-import {CloseIcon} from "@chakra-ui/icons";
+import {AddIcon, CloseIcon, InfoIcon, SearchIcon} from "@chakra-ui/icons";
 import Select from "react-select";
 import {GradientBar} from "../../components/GradientBar";
 import {useNavigate} from "react-router";
 
 const urlProtocolli = "protocolli";
 const urlUtenti = "utenti";
+const urlSchedaAllenamento = "/trainingcards"
+const urlSchedaAlimentazione = "/dietcards"
 
 const Create = () => {
   const fetchContext = useContext(FetchContext);
@@ -37,6 +57,13 @@ const Create = () => {
   const [selectedSchedaAlimentare, setselectedSchedaAlimentare] = useState(null);
   const { register, handleSubmit, setValue, formState: { errors} } = useForm();
   const [toastMessage, setToastMessage] = useState(undefined);
+  const [search, setSearch] = useState("");
+
+  const {isOpenSchedaAlimentare, onOpenSchedaAlimentare, onCloseSchedaAlimentare} = useDisclosure()
+
+  const [listSchedeAlimentari, setListSchedeAlimentari] = useState();
+  const [listSchedeAllenamento, setListSchedeAllenamento] = useState();
+
   const toast = useToast({
     duration: 3000,
     isClosable: true,
@@ -63,26 +90,9 @@ const Create = () => {
     }
   }, [toastMessage, toast]);
 
-  const onDropAllenamento = useCallback(acceptedSchedaAllenamento => {
-    console.log(acceptedSchedaAllenamento)
-    setValue("schedaAllenamento", acceptedSchedaAllenamento);
-  }, [setValue])
-  const onDropAlimentare = useCallback(acceptedSchedaAlimentare => {
-    console.log(acceptedSchedaAlimentare)
-    setValue("schedaAlimentare", acceptedSchedaAlimentare);
-  }, [setValue])
-  const {
-    acceptedFiles: acceptedSchedaAllenamento,
-    getRootProps: getRootPropsAllenamento,
-    getInputProps: getInputPropsAllenamento,
-    isDragActive: isDragActiveAllenamento
-  } = useDropzone({ onDrop: onDropAllenamento, maxFiles: 1 })
-  const {
-    acceptedFiles: acceptedSchedaAlimentare,
-    getRootProps: getRootPropsAlimentare,
-    getInputProps: getInputPropsAlimentare,
-    isDragActive: isDragActiveAlimentare
-  } = useDropzone({ onDrop: onDropAlimentare, maxFiles: 1 })
+  const onChange = (e) => {
+    setSearch(e.target.value); // e evento target chi lancia l'evento e il value è il valore
+  }
 
   const onSubmit = async (values) => {
     const formData = new FormData();
@@ -104,15 +114,10 @@ const Create = () => {
   }
 
   useEffect(() => {
-    setselectedSchedaAllenamento(acceptedSchedaAllenamento[0]);
-  }, [acceptedSchedaAllenamento]);
-
-  useEffect(() => {
     setisLoading(true);
     const getUsers = async () => {
       try {
         const { data: { data: { clienti } } } = await fetchContext.authAxios(urlUtenti);
-        console.log(clienti);
         setOptions(
           clienti.map((e) => {
             return { value: e.id, label: e.nome };
@@ -120,16 +125,40 @@ const Create = () => {
         );
         setisLoading(false);
       } catch (error) {
-        console.log(error)
         setToastMessage({title:"Error",body:error.message,stat:"error"})
       }
     }
+    const loadlistaSchedeAllenamento = async () => {
+      try {
+        const {data} = await fetchContext.authAxios("schedaAllenamento/getMySchedeAllenamento");
+        setListSchedeAllenamento(data.data.response);
+      } catch (error) {
+        setToastMessage({title: "Errore", body: error.message, stat: "error"});
+      }
+    }
+    const loadlistaSchedeAlimentari = async () => {
+      try {
+        const { data } = await fetchContext.authAxios("schedaalimentare/getMySchedeAlimentari");
+        setListSchedeAlimentari(data.data.response);
+        setisLoading(false); //viene settato a false per far capire di aver caricato tutti i dati
+      } catch (error) {
+        setToastMessage({title:"Errore", body:error.message, stat:"error"});
+      }
+
+    }
     getUsers();
+    loadlistaSchedeAllenamento();
+    loadlistaSchedeAlimentari();
   }, [fetchContext])
 
-  useEffect(() => {
-    setselectedSchedaAlimentare(acceptedSchedaAlimentare[0]);
-  }, [acceptedSchedaAlimentare]);
+
+  function selectSchedaAlimentare(idSchedaAlimentare){
+
+  }
+
+  function selectSchedaAllenamento(idSchedaAllenamento){
+
+  }
 
   //Verifica se una data inserita è precedenta alla odierna
   function isValidDate(value) {
@@ -137,8 +166,6 @@ const Create = () => {
     date.setHours(23, 59, 59, 59);
     return (!isNaN(Date.parse(value)) && (new Date(value) > date) ? true : "Inserisci una data valida");
   }
-
-
 
   return (
     <>
@@ -148,6 +175,90 @@ const Create = () => {
         </Flex>
         <Box bg={"white"} roundedTop={20} minW={{ base: "100%", xl: "100%" }} h={"full"}>
           <GradientBar />
+
+          <Modal isOpen={isOpenSchedaAlimentare} onClose={onCloseSchedaAlimentare} isCentered={true} size={"5xl"}>
+            <ModalOverlay/>
+            <ModalContent>
+              <ModalHeader fontSize={'3xl'} textAlign={"center"}>Aggiungi alimenti alla
+                scheda</ModalHeader>
+              <ModalCloseButton/>
+              <ModalBody align={"center"}>
+                <Flex justify="center">
+                  <HStack align="center">
+                    <HStack>
+                      {!isLoading && listSchedeAlimentari && (<Flex wrap={"wrap"} p={5}>
+                        <Box bg={"white"} roundedTop={20}
+                             minW={{base: "100%", xl: "100%"}}
+                             h={"full"}>
+                          <GradientBar/>
+                          <Box pl={10} pr={10} pb={5} pt={5}>
+                            <HStack>
+                              <InputGroup>
+                                <InputLeftElement
+                                    pointerEvents="none"
+                                    children={<SearchIcon color="gray.300" />}
+                                />
+                                <Input
+                                    className="SearchInput"
+                                    type="text"
+                                    onChange={onChange}
+                                    placeholder="Search"
+                                />
+                              </InputGroup>
+                            </HStack>
+                            {/* Barra di ricerca*/}
+                            {listSchedeAlimentari.schede_alimentari.length > 0 ? (
+                                <>
+                                  <Text fontSize="xl" my={5}>
+                                    Lista dei protocolli
+                                  </Text>
+                                  <Table variant={"striped"} colorScheme={"gray"} size="md">
+                                    <TableCaption>Lista Schede Alimentari</TableCaption>
+                                    <Thead bg="fitdiary.100">
+                                      <Tr>
+                                        <Th>ID</Th>
+                                        <Th>Nome</Th>
+                                        <Th>Kcal</Th>
+                                        <Th>Azione</Th>
+                                      </Tr>
+                                    </Thead>
+                                    <Tbody>
+                                      {listSchedeAlimentari.schede_alimentari.map(
+                                          (scheda) =>
+                                              (scheda.id === parseInt(search) ||
+                                                  search === "") && (
+                                                  <Tr key={scheda.id}>
+                                                    <Td>{scheda.id}</Td>
+                                                    <Td>{scheda.nome}</Td>
+                                                    <Td>{scheda.kcalAssunte}</Td>
+                                                    <Td>
+                                                      <ReactLink to={urlSchedaAlimentazione + "/" + scheda.id}>
+                                                        <InfoIcon />
+                                                      </ReactLink>
+                                                    </Td>
+                                                  </Tr>
+                                              )
+                                      )}
+                                    </Tbody>
+                                  </Table>
+                                </>
+                            ) : (
+                                <Heading py={5} textAlign={"center"}>
+                                  Non c'è niente qui...
+                                </Heading>
+                            )}
+                          </Box>
+                        </Box>
+                      </Flex>)}
+                    </HStack>
+                  </HStack>
+                </Flex>
+              </ModalBody>
+              <ModalFooter alignItems={"center"}>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+
           <VStack spacing={3} alignItems="center" pb={5} mt={5}>
             <form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
               <SimpleGrid columns={2} columnGap={5} rowGap={5} pl={[0, 5, 10]} pr={[0, 5, 10]} w="full">
@@ -190,10 +301,15 @@ const Create = () => {
                         <GiMeal size={"40%"} color="#00a9ff" />
                       </Flex>
                       <Flex direction="column" alignItems="center">
-                        <Button colorScheme="fitdiary" type="submit" w="80%" margin="0.1rem">
-                          Crea nuova
-                        </Button>
-                        <Button background="#8BC0FF" color="white" type="submit" w="80%" margin="0.1rem">
+                          <Button colorScheme="fitdiary"  w="80%" margin="0.1rem">
+                            <ReactLink to={"../DietCards/create"} >
+                            Crea nuova
+                            </ReactLink>
+                          </Button>
+                        <Button background="#8BC0FF" color="white" w="80%" margin="0.1rem"
+                                onClick={() => {
+                                  onOpenSchedaAlimentare();
+                                }}>
                           Seleziona esistente
                         </Button>
                       </Flex>
@@ -219,10 +335,12 @@ const Create = () => {
                         <IoIosFitness size={"40%"} color="#00a9ff" />
                       </Flex>
                       <Flex direction="column" alignItems="center">
-                        <Button colorScheme="fitdiary" type="submit" w="80%" margin="0.1rem">
-                          Crea nuova
+                        <Button colorScheme="fitdiary" w="80%" margin="0.1rem">
+                          <ReactLink to={"../TrainingCards/create"} >
+                            Crea nuova
+                          </ReactLink>
                         </Button>
-                        <Button background="#8BC0FF" color="white" type="submit" w="80%" margin="0.1rem">
+                        <Button background="#8BC0FF" color="white" w="80%" margin="0.1rem">
                           Seleziona esistente
                         </Button>
                       </Flex>
